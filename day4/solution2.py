ca50a21c7a267b22b4fb6ebace58695ec7aa8834
mauricioinaz <mauricio.glez@gmail.com>
2019-12-04 09:41:23 -0500

    Second solution for day4 part2

---
+++ day4/solution2.py
@@ -0,0 +1,29 @@
+def incremental(numb):
+    numb_str = ''.join(sorted(str(numb)))
+    return str(numb) == numb_str
+
+
+def has_doubles(number):
+    numb = str(number)
+    for n in numb:
+        if numb.count(n) == 2:
+            return True
+    return False
+
+
+def main():
+    lower_limit = 359282
+    upper_limit = 820401
+
+    n = lower_limit
+    number_of_passwords = 0
+    while n < upper_limit:
+        if incremental(n) and has_doubles(n):
+            number_of_passwords += 1
+        n += 1
+
+    print(number_of_passwords)
+
+
+if __name__ == "__main__":
+    main()
